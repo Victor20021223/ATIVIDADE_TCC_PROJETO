6a5edbe7f2323ba35f39c1506de935f7deef33a1
VictorHugo <109436503+Victor20021223@users.noreply.github.com>
2023-12-04 23:12:40 -0300

    SEgunda

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,11 @@
+const e = require("express");
 const express = require("express");
 const router = express.Router();
 
 router.get('/', (rep, res) => {
     res.render("admin/ControleEmpresa")
 });
+
+e
 
 module.exports = router;
