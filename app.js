879bfe5f28ab56cbdd7fc95a85466ef0851f514a
VictorHugo <109436503+Victor20021223@users.noreply.github.com>
2023-12-05 22:58:51 -0300

    Terca

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 //Config
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Conexão com Banco 
@@ -20,6 +20,19 @@ app.use(express.static('public'));
 //Rotas USER
 
 //Rotas GET
+
+app.get('/cards', (req, res) => {
+    const query = 'SELECT * FROM empresas';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro ao executar a consulta:', err);
+            res.status(500).send('Erro interno do servidor');
+            return;
+        }
+        res.json(results);
+    });
+});
+
 app.get('/', async (req, res) => {
     res.sendFile(__dirname + "/src/index.html")
 });
@@ -52,32 +65,32 @@ app.get('/addEmpresa', async (req, res) => {
 //Rotas POST
 app.post('/add', async (req, res) => {
     await User.create({
-        NOME:req.body.nomeUsuario,
-        CELULAR:req.body.celularUsuario,
-        EMAIL:req.body.emailUsuario,
-        SENHA:req.body.senhaUsuario,
-        GENERO:req.body.generoUsuario
+        NOME: req.body.nomeUsuario,
+        CELULAR: req.body.celularUsuario,
+        EMAIL: req.body.emailUsuario,
+        SENHA: req.body.senhaUsuario,
+        GENERO: req.body.generoUsuario
     })
-    res.sendFile(__dirname+"/src/index.html")
-    
+    res.sendFile(__dirname + "/src/index.html")
+
 });
 
-app.post('/addEmpresa', async (req, res) =>{
+app.post('/addEmpresa', async (req, res) => {
     await Empresa.create({
-        NOME:req.body.nomeEmpresa,
-        CNPJ:req.body.CNPJ,
-        EMAIL:req.body.emailEmpresa,
-        SENHA:req.body.senhaEmpresa,
-        TELEFONE:req.body.telefoneEmpresa,
-        LOGRADOURO:req.body.logradouro,
-        NUMERO:req.body.numero,
-        BAIRRO:req.body.bairro,
-        IMAGEM:req.body.imagem
+        NOME: req.body.nomeEmpresa,
+        CNPJ: req.body.CNPJ,
+        EMAIL: req.body.emailEmpresa,
+        SENHA: req.body.senhaEmpresa,
+        TELEFONE: req.body.telefoneEmpresa,
+        LOGRADOURO: req.body.logradouro,
+        NUMERO: req.body.numero,
+        BAIRRO: req.body.bairro,
+        IMAGEM: req.body.imagem
     })
-    res.sendFile(__dirname+'/src/ControleEmpresa.html')
+    res.sendFile(__dirname + '/src/ControleEmpresa.html')
 })
 
-app.post("uplods-image", async (req, res) =>{
+app.post("uplods-image", async (req, res) => {
 
 })
 
