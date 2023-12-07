8cf69ba946cb945e886f0cb2dc79a2eaf5b0f72a
VictorHugo <109436503+Victor20021223@users.noreply.github.com>
2023-12-07 18:45:36 -0300

    Quarta

--- app.js
+++ app.js
@@ -5,46 +5,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const session = require('express-session');
 const flash = require('connect-flash');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 
-
-//Teste
-passport.use(
-    new LocalStrategy({ usernameField: 'username' }, async (username, password, done) => {
-      try {
-        const user = await User.findOne({ where: { username: username } });
-  
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-  
-        const isValidPassword = await bcrypt.compare(password, user.password);
-  
-        if (!isValidPassword) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-  
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
-    })
-  );
-  
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findByPk(id);
-      done(null, user);
-    } catch (error) {
-      done(error);
-    }
-  });
 
 //Config
 app.use(express.json());
